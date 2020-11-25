57b60fcfd4be4a988b3b460d5ad1d273dc152ce3
jayweiboy <64019748+jayweiboy@users.noreply.github.com>
2020-11-25 08:23:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,6 @@
 #=============================================================
 
 # fw876/helloworld
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
